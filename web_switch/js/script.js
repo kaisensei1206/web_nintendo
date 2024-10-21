15ef77af94e34d7e2320d729b1c49839f9fbedd2
kaisensei1206 <willyfan2230@gmail.com>
2024-10-21 18:52:18 +0800

    修正了某些錯誤

---
+++ web_switch/js/script.js
@@ -0,0 +1,29 @@
+$(".item-1").hover(function(){
+    $(".span-1").css("color", "#E60012");
+    }, function() {
+    $(".span-1").css("color", "black");    
+});
+
+$(".item-2").hover(function(){
+    $(".span-2").css("color", "#E60012");
+    }, function() {
+    $(".span-2").css("color", "black");    
+});
+
+$(".item-3").hover(function(){
+    $(".span-3").css("color", "#E60012");
+    }, function() {
+    $(".span-3").css("color", "black");    
+});
+
+$(".item-4").hover(function(){
+    $(".span-4").css("color", "#E60012");
+    }, function() {
+    $(".span-4").css("color", "black");    
+});
+
+$(".item-5").hover(function(){
+    $(".span-5").css("color", "#E60012");
+    }, function() {
+    $(".span-5  ").css("color", "black");    
+});
